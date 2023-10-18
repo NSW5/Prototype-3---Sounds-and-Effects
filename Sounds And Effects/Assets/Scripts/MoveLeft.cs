88718fd684bcd7f846b08313cd2e40a1129bcce0
NSW5 <146126932+NSW5@users.noreply.github.com>
2023-10-18 12:04:08 -0700

    Finished 3.3.3

--- Sounds And Effects/Assets/Scripts/MoveLeft.cs
+++ Sounds And Effects/Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float _speed = 30f;
     private PlayerController playerControllerScript;
+    private float _leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,9 @@ public class MoveLeft : MonoBehaviour
             transform.Translate(Vector3.left * _speed * Time.deltaTime);
         }
         
+        if (transform.position.x < _leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
